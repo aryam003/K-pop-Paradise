d68cb087a5c0c6380829615bc8fe9dcebe5d559e
aryam003 <aryamarya537@gmail.com>
2025-02-01 22:31:25 +0530

    work

--- Kpop_Paradise/app/views.py
+++ Kpop_Paradise/app/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from . models import *
+import os
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import HttpResponse
 
 # Create your views here.
+def shop_home(req):
+     return render(req,'shop/home.html')
