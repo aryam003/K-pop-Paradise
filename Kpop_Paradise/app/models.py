5e232a338ad3c9efcc05ab75f41907c99cc08605
aryam003 <aryamarya537@gmail.com>
2025-02-03 13:40:39 +0530

    work

--- Kpop_Paradise/app/models.py
+++ Kpop_Paradise/app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Concert(models.Model):
+    name = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    data = models.DateTimeField()
+    
