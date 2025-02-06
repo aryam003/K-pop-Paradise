1829addfb02117ce4f1a102b3ddd804cde860845
aryam003 <aryamarya537@gmail.com>
2025-02-06 09:22:18 +0530

    work

--- Kpop_Paradise/app/urls.py
+++ Kpop_Paradise/app/urls.py
@@ -8,7 +8,8 @@ urlpatterns=[
 
 
     path('shop_home',views.shop_home),
-
+    path('band/<int:id>/', views.shop_concert_list, name='concert_list'),
+    
 
     path('user_home',views.user_home),
     # path('band/<id>', views.concert_list, name='concert_list'),
